47ef6066960964eabd7d25df0011ca0b8277e3a1
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-22 10:10:25 -0400

    Bedtime stories done

---
+++ Mod01/BedtimeStories/src/main/java/com/pluralsight/BedtimeStories.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BedtimeStories {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the name of the story:" );
+        String FileName = input.nextLine();
+
+        File story = new File(FileName);
+        int lineNumber =1;
+
+        try (Scanner filescanner = new Scanner(story)){
+            while (filescanner.hasNextLine()){
+                String line = filescanner.nextLine();
+                System.out.printf("%2d: %s%n", lineNumber++, line);
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("The file \"" + FileName + "\" was not found. Please check the name and try again.");
+        }
+
+
+    }
+
+}
