3790b3b87a857438e0865016200ee1b8fbf66023
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-23 13:45:42 -0400

    working

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -13,31 +13,53 @@ public class PayrollCalculator {
         System.out.print("Enter the name of the payroll file to create: ");
         String outputFilePath = scanner.nextLine().trim();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
-             PrintWriter writer = new PrintWriter(new FileWriter(outputFilePath))) {
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(inputFilePath));
+                FileWriter fw = new FileWriter(outputFilePath);
+                BufferedWriter bufWriter = new BufferedWriter(fw)
+        ) {
 
-            writer.println("id|name|gross pay");
+            bufWriter.write("id|name|gross pay");
+            bufWriter.newLine();
+
+            BufferedReader readar;
+            reader.readLine();
+
 
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] items = line.split("\\|");
                 if (items.length != 4) continue;// skip lines
 
-                int id = Integer.parseInt(items[0]);
-                String name = items[1];
-                double hours = Double.parseDouble(items[2]);
-                double rate = Double.parseDouble(items[3]);
+                try {
+                    int id = Integer.parseInt(items[0]);
+                    String name = items[1];
+                    double hours = Double.parseDouble(items[2]);
+                    double rate = Double.parseDouble(items[3]);
 
-                Employee employee = new Employee(id, name, hours, rate);
+                    Employee employee = new Employee(id, name, hours, rate);
 
-                writer.printf("%d|%s|%.2f%n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+                    String outputLine = String.format("%d|%s|%.2f",
+                            employee.getEmployeeId(),
+                            employee.getName(),
+                            employee.getGrossPay());
+
+                    bufWriter.write(outputLine);
+                    bufWriter.newLine();
+
+                } catch (NumberFormatException e) {
+                    System.err.println("Skipping invalid numeric line: " + line);
+                }
             }
+            bufWriter.flush();
+            System.out.println("Payroll file created: " + outputFilePath);
 
-            System.out.println("Payroll file ceated: " + outputFilePath);
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
+}
+
 
 
 
